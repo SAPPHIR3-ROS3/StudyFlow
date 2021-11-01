91d724c7861738d4ef61b7a8dd95e70c5ea47372
Patrizio Onida <patriziosimone@hotmail.it>
2021-11-01 17:55:52 +0100

    added function that fromats second properly fixed fault condition in playpause added a check to ensure the user is in core state implemented skiptobreak

--- StudyFlow.py
+++ StudyFlow.py
@@ -99,6 +99,12 @@ class App(Tk):
         self.NextSessionButton = Button(self.Win, text = 'Go to next session', command = lambda : self.NextSession())
         self.NextSessionButton.place(anchor = 'n', relx = 0.825, rely = 0.825, relwidth = 0.3, relheight = 0.1) # placing the
 
+    def ConvertedCounter(self):
+        Seconds = str(self.Counter % 60).zfill(2)
+        Minutes = str((self.Counter - int(Seconds)) // 60).zfill(2)
+
+        return f'{Minutes}:{Seconds}'
+
     def StartStop(self): # this function start/stop the sessions of study
         if self.InSession: # check if the session is already active
             self.Counter = 0 # reset the timer
@@ -116,7 +122,7 @@ class App(Tk):
             self.Sessions[1].config(state = 'enabled') # enabling input on session field
             self.InSession = False # reset the user to not in session
             self.InCore = False # reset the user to nt in core part of session
-            self.InPause = False # rest the user to not in pause during session
+            self.InPause = False # reset the user to not in pause during session
             self.InBreak = False # reset the user to not in break part
 
         else:
@@ -135,23 +141,42 @@ class App(Tk):
             self.InCore = True # put the user in core part of the session
             self.TimeCount() # start the timer
 
-    def PlayPause(self):
-        if self.InSession:
-            if self.InPause:
-                self.InPause = False
-            else:
-                self.InPause = True
-            self.TimeCount()
+    def PlayPause(self): # this function play or pause the timer
+        if self.InSession or (self.InBreak and not self.InSession): # check if user is in session or in break
+            if self.InPause: # check if the user is in pause
+                self.InPause = False # reverse the pause state
+            else: # if the user is not in pause
+                self.InPause = True # reverse the pause state
 
     def TimeCount(self):
         if self.InSession:
             if not self.InPause:
+                if self.Counter > self.ActiveMinTime:
+                    self.InCore = False
+
                 self.Counter += 1
-                Seconds = str(self.Counter % 60).zfill(2)
-                Minutes = str((self.Counter - int(Seconds)) // 60).zfill(2)
-                #print(self.Counter)
-                self.TimerValue.set(f'{Minutes}:{Seconds}')
-                self.after(1000, self.TimeCount)
+                self.TimerValue.set(self.ConvertedCounter())
+
+                if self.Counter < self.ActiveMaxTime + self.ActiveOverTime:
+                    self.after(1000, self.TimeCount)
+                else:
+                    self.InSession = False
+                    self.Counter = 0
+
+        if self.InBreak and not self.InSession:
+            if not self.InPause:
+                self.Counter += 1
+
+                if self.Counter < self.ActiveBreakTime:
+                    self.TimerValue.set(self.ConvertedCounter())
+                    self.after(1000, self.TimeCount)
+                else:
+                    self.InBreak = False
+                    self.Counter = 0
+
+        if not self.InSession and not self.InSession:
+            self.after(1000, self.TimeCount())
+
 
     def ResetValues(self): # this function reset values to defalut
         self.MinTime[1].delete(0,'end')
@@ -167,7 +192,9 @@ class App(Tk):
 
     def SkipToBreak(self):
         if not self.InCore:
-            pass
+            self.InSession = False
+            self.InBreak = True
+            self.Counter = 0
 
     def EndSession(self):
         pass
